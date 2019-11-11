75c3e5d2819d66a7ca27a6235859f47c5416c534
vaspap1790 <vaspap1790@gmail.com>
2019-11-11 11:05:27 +0200

    logout implementation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,29 @@
 const User = require("../models/User");
 
-exports.login = function () {
+exports.login = function (req, res) {
+
+    const user = new User(req.body);
+    user.login()
+        .then(function (result) {
+            req.session.user = { username: user.data.username };
+            req.session.save(function () {
+                res.redirect('/');
+            });
+        })
+        .catch(function (err) {
+            req.flash('errors', err);
+            req.session.save(function () {
+                res.redirect('/');
+            });
+        });
 
 };
 
-exports.logout = function () {
+exports.logout = function (req, res) {
 
+    req.session.destroy(function () {
+        res.redirect('/');
+    });
 };
 
 exports.register = function (req, res) {
@@ -21,5 +39,10 @@ exports.register = function (req, res) {
 };
 
 exports.home = function (req, res) {
-    res.render("home-guest");
+    if (req.session.user) {
+        res.render("home-dashboard", { username: req.session.user.username });
+    }
+    else {
+        res.render("home-guest", { errors: req.flash('errors') });
+    }
 };
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
-const usersCollection = require("../db").collection("users");
+const bcrypt = require('bcryptjs');
+const usersCollection = require("../db").db().collection("users");
 const validator = require("validator");
 
 let User = function (data) {
@@ -42,8 +43,8 @@ User.prototype.validate = function () {
     if (this.data.password.length > 0 && this.data.password.length < 4) {
         this.errors.push("Password should be at least 4 characters");
     };
-    if (this.data.password.length > 100) {
-        this.errors.push("Password cannot exceed 100 characters");
+    if (this.data.password.length > 50) {
+        this.errors.push("Password cannot exceed 50 characters");
     };
     if (this.data.username.length > 0 && this.data.username.length < 3) {
         this.errors.push("Username should be at least 3 characters");
@@ -53,13 +54,32 @@ User.prototype.validate = function () {
     };
 };
 
+User.prototype.login = function () {
 
+    return new Promise((resolve, reject) => {
+        this.cleanUp();
+        usersCollection.findOne({ username: this.data.username })
+            .then((attemptedUser) => {
+                if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
+                    resolve("Congatulations");
+                }
+                else {
+                    reject("Invalid username/password");
+                }
+            })
+            .catch(function () {
+                reject("Please try again later.");
+            });
+    });
+};
 
 User.prototype.register = function () {
 
     this.cleanUp();
     this.validate();
     if (!this.errors.length) {
+        let salt = bcrypt.genSaltSync(10);
+        this.data.password = bcrypt.hashSync(this.data.password, salt);
         usersCollection.insertOne(this.data);
     };
 };
