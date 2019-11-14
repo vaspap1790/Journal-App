b035efabf3ee719443b2f37e4214bad17e522e13
vaspap1790 <vaspap1790@gmail.com>
2019-11-14 10:41:55 +0200

    update post

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,23 +1,76 @@
 const Post = require('../models/Post')
 
-exports.viewCreateScreen = function(req, res) {
+exports.viewCreateScreen = function (req, res) {
   res.render('create-post')
 }
 
-exports.create = function(req, res) {
+exports.create = function (req, res) {
   let post = new Post(req.body, req.session.user._id)
-  post.create().then(function() {
+  post.create().then(function () {
     res.send("New post created.")
-  }).catch(function(errors) {
+  }).catch(function (errors) {
     res.send(errors)
   })
 }
 
-exports.viewSingle = async function(req, res) {
+exports.viewSingle = async function (req, res) {
   try {
-    let post = await Post.findSingleById(req.params.id)
-    res.render('single-post-screen', {post: post})
+    let post = await Post.findSingleById(req.params.id, req.visitorId)
+    res.render('single-post-screen', { post: post })
   } catch {
     res.render('404')
   }
 }
+
+exports.viewEditScreen = async function (req, res) {
+  try {
+    let post = await Post.findSingleById(req.params.id)
+    res.render("edit-post", { post: post })
+  } catch {
+    res.render("404")
+  }
+}
+
+exports.viewEditScreen = async function (req, res) {
+
+  try {
+    let post = await Post.findSingleById(req.params.id);
+    res.render('edit-post', { post: post });
+  }
+  catch{
+    res.render('404');
+  }
+
+};
+
+exports.edit = function (req, res) {
+
+  let post = new Post(req.body, req.visitorId, req.params.id);
+  post.update().then((status) => {
+
+    if (status == "success") {
+
+      req.flash('success', 'Post successfully updated');
+      req.session.save(function () {
+        res.redirect(`/post/${req.params.id}/edit`);
+      })
+    }
+    else {
+      post.errors.forEach(function (error) {
+        req.flash("errors", error)
+      })
+      req.session.save(function () {
+        res.redirect(`/post/${req.params.id}/edit`)
+      })
+    };
+
+  })
+    .catch(() => {
+
+      req.flash("errors", "You do not have permission to perform that action.");
+      req.session.save(function () {
+        res.redirect('/');
+      })
+    });
+
+};
--- models/Post.js
+++ models/Post.js
@@ -2,15 +2,16 @@ const postsCollection = require('../db').db().collection("posts")
 const ObjectID = require('mongodb').ObjectID
 const User = require('./User')
 
-let Post = function(data, userid) {
+let Post = function (data, userid, requestedPostId) {
   this.data = data
   this.errors = []
   this.userid = userid
+  this.requestedPostId = requestedPostId
 }
 
-Post.prototype.cleanUp = function() {
-  if (typeof(this.data.title) != "string") {this.data.title = ""}
-  if (typeof(this.data.body) != "string") {this.data.body = ""}
+Post.prototype.cleanUp = function () {
+  if (typeof (this.data.title) != "string") { this.data.title = "" }
+  if (typeof (this.data.body) != "string") { this.data.body = "" }
 
   // get rid of any bogus properties
   this.data = {
@@ -21,12 +22,12 @@ Post.prototype.cleanUp = function() {
   }
 }
 
-Post.prototype.validate = function() {
-  if (this.data.title == "") {this.errors.push("You must provide a title.")}
-  if (this.data.body == "") {this.errors.push("You must provide post content.")}
+Post.prototype.validate = function () {
+  if (this.data.title == "") { this.errors.push("You must provide a title.") }
+  if (this.data.body == "") { this.errors.push("You must provide post content.") }
 }
 
-Post.prototype.create = function() {
+Post.prototype.create = function () {
   return new Promise((resolve, reject) => {
     this.cleanUp()
     this.validate()
@@ -44,22 +45,62 @@ Post.prototype.create = function() {
   })
 }
 
-Post.reusablePostQuery = function(uniqueOperations) {
-  return new Promise(async function(resolve, reject) {
+Post.prototype.update = function () {
+
+  return new Promise(async (resolve, reject) => {
+    try {
+      let post = await Post.findSingleById(this.requestedPostId, this.userid)
+      if (post.isVisitorOwner) {
+
+        let status = await this.actuallyUpdate();
+        resolve(status);
+      }
+      else {
+        reject();
+      };
+    }
+    catch{
+      reject();
+    };
+  });
+};
+
+Post.prototype.actuallyUpdate = function () {
+
+  return new Promise(async (resolve, reject) => {
+    this.cleanUp();
+    this.validate();
+    if (!this.errors.lengh) {
+      await postsCollection.findOneAndUpdate({ _id: new ObjectID(this.requestedPostId) }, { $set: { title: this.data.title, body: this.data.body } })
+      resolve("success")
+    }
+    else {
+      resolve('failure')
+    }
+  })
+}
+
+Post.reusablePostQuery = function (uniqueOperations, visitorId) {
+  return new Promise(async function (resolve, reject) {
     let aggOperations = uniqueOperations.concat([
-      {$lookup: {from: "users", localField: "author", foreignField: "_id", as: "authorDocument"}},
-      {$project: {
-        title: 1,
-        body: 1,
-        createdDate: 1,
-        author: {$arrayElemAt: ["$authorDocument", 0]}
-      }}
+      { $lookup: { from: "users", localField: "author", foreignField: "_id", as: "authorDocument" } },
+      {
+        $project: {
+          title: 1,
+          body: 1,
+          createdDate: 1,
+          authorId: "$author",
+          author: { $arrayElemAt: ["$authorDocument", 0] }
+        }
+      }
     ])
 
     let posts = await postsCollection.aggregate(aggOperations).toArray()
 
     // clean up author property in each post object
-    posts = posts.map(function(post) {
+    posts = posts.map(function (post) {
+      post.isVisitorOwner = post.authorId.equals(visitorId)
+
       post.author = {
         username: post.author.username,
         avatar: new User(post.author, true).avatar
@@ -72,16 +113,16 @@ Post.reusablePostQuery = function(uniqueOperations) {
   })
 }
 
-Post.findSingleById = function(id) {
-  return new Promise(async function(resolve, reject) {
-    if (typeof(id) != "string" || !ObjectID.isValid(id)) {
+Post.findSingleById = function (id, visitorId) {
+  return new Promise(async function (resolve, reject) {
+    if (typeof (id) != "string" || !ObjectID.isValid(id)) {
       reject()
       return
     }
-    
+
     let posts = await Post.reusablePostQuery([
-      {$match: {_id: new ObjectID(id)}}
-    ])
+      { $match: { _id: new ObjectID(id) } }
+    ], visitorId)
 
     if (posts.length) {
       console.log(posts[0])
@@ -92,10 +133,10 @@ Post.findSingleById = function(id) {
   })
 }
 
-Post.findByAuthorId = function(authorId) {
+Post.findByAuthorId = function (authorId) {
   return Post.reusablePostQuery([
-    {$match: {author: authorId}},
-    {$sort: {createdDate: -1}}
+    { $match: { author: authorId } },
+    { $sort: { createdDate: -1 } }
   ])
 }
 
